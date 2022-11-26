9029ba2040036ad1d91133ea5877122cec179633
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-11-26 22:57:25 +0300

    initial commit

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,11 @@
 package com.epam.utils;
 
+import static org.apache.commons.lang3.math.NumberUtils.isCreatable;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (!isCreatable(str))
+            return false;
+        return !str.contains("-") && !str.equals("0") && !str.trim().equals("");
     }
 }
